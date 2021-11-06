11287e1bdd31a69c116a27f7b1759d729693a7b1
AlexanderIlinykhDev <AlexanderIlinykhDev@yandex.ru>
2021-11-07 00:09:40 +0500

    header is maked up withour additioonal components creation

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components'
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
+import './app-header.css';
 
 class AppHeader extends React.Component {
     constructor(props) {
@@ -17,9 +21,52 @@ class AppHeader extends React.Component {
     render() {
         //const className = this.state.theme === 'dark' ? 'Btn-dark' : 'Btn-light';
         return (
-            <>
 
+            <>
                 <header>
+                    <div className="header__container-compensate">
+                        <div className="header__container">
+                            <div className="header__left-links">
+                                <div className="menu__item menu__item-first pt-5 pr-5 pb-5 mt-4 mb-4 mr-2">
+                                    <div className="mr-2">
+                                        <BurgerIcon type="primary" className="mr-2" />
+                                    </div>
+                                    <a href="" className="menu__link">
+                                        <span className="text text_type_main-default">
+                                            Конструктор
+                                        </span>
+                                    </a>
+                                </div>
+                                <div className="menu__item menu__item-second p-5 mt-4 mb-4">
+                                    <div className="mr-2">
+                                        <ListIcon type="primary" />
+                                    </div>
+                                    <a href="" className="menu__link">
+                                        <span className="text text_type_main-default">
+                                            Лента заказов
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                            <Logo className="menu__logo" />
+                            <div className="header__right-links">
+                                <div className="menu__item menu__item-third pt-5 pl-5 pb-5 mt-4 mb-4">
+                                    <div className="mr-2">
+                                        <ProfileIcon type="primary" />
+                                    </div>
+                                    <a href="" className="menu__link menu__link-3">
+                                        <span className="text text_type_main-default">
+                                            Личный кабинет
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header >
+
+                {/*
+                <div>
                     <nav>
                         <Button type="secondary" size="medium">
                             Конструктор
@@ -28,24 +75,15 @@ class AppHeader extends React.Component {
                             Лента заказов
                         </Button>
                         <Logo />
-                        <Button type="secondary" size="medium">
+                        <Button id="personalCabinet" type="secondary" size="medium">
                             Личный кабинет
                         </Button>
                     </nav>
-                </header>
-                <h1>
-                    Василий и Александр - крутые обработчики
-                </h1>
-                <Button type="primary" size="small" onClick={this.showGreeting}>
-                    Нажми на меня если прогер
-                </Button>
-                <CurrencyIcon type="primary" />
-                <Logo />
-                <p className="text text_type_main-large">
-                    The quick brown fox jumps over the lazy dog.
-                </p>
+                </div>
+            */}
 
             </>
+
         )
     }
 }
