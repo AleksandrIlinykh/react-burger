f64055a2532dffa829cae4eef2bb290291cb14b0
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2021-12-25 23:55:36 +0500

    Беру id из useParams

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,60 +1,74 @@
 import ingredientDetailsStyles from './ingredient-details.module.css'
 import { ingredientDetailsTypes } from '../../utils/types'
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
 
 function IngredientDetails(props) {
-  const { ingredient } = useSelector((store) => ({
-    ingredient: store.burgerIngredients.ingredients.filter(
-      (ingredient) =>
-        ingredient._id === store.ingredientDetails.ingredientDetailsId
-    )[0],
-  }));
+  let { ingredientId } = useParams();
+  ingredientId = ingredientId.slice(0, -1);
+  /*
+  useEffect(() => {
+    dispatch(getBurgerIngredients());
+  }, [dispatch]);
+*/
+  const ingredient = useSelector(
+    (store) =>
+      store.burgerIngredients.ingredients.filter(
+        (ingredient) => ingredient._id === ingredientId
+      )[0]
+  );
+
   return (
-    <section className={ingredientDetailsStyles.ingredientdetails}>
-      <h2
-        className={
-          (ingredientDetailsStyles.header =
-            " text text_type_main-large mt-10 ml-10 mr-10")
-        }
-      >
-        Детали ингредиента
-      </h2>
-      <div className={ingredientDetailsStyles.content}>
-        <img src={ingredient.image_large} alt="Тут картинка из props" />
-        <p className="text text_type_main-medium mt-4">{ingredient.name}</p>
-        <ul
-          className={
-            ingredientDetailsStyles.parameters +
-            " text_color_inactive mt-8 mb-15"
-          }
-        >
-          <li className={ingredientDetailsStyles.parameter}>
-            <p className="text text_type_main-small">Каллории, ккал</p>
-            <p className="text text_type_digits-default mt-2">
-              {ingredient.calories}
-            </p>
-          </li>
-          <li className={ingredientDetailsStyles.parameter}>
-            <p className="text text_type_main-small">Белки, г</p>
-            <p className="text text_type_digits-default mt-2">
-              {ingredient.proteins}
-            </p>
-          </li>
-          <li className={ingredientDetailsStyles.parameter}>
-            <p className="text text_type_main-small">Жиры, г</p>
-            <p className="text text_type_digits-default mt-2">
-              {ingredient.fat}
-            </p>
-          </li>
-          <li className={ingredientDetailsStyles.parameter}>
-            <p className="text text_type_main-small">Углеводы, г</p>
-            <p className="text text_type_digits-default mt-2">
-              {ingredient.carbohydrates}
-            </p>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <>
+      {ingredient && (
+        <section className={ingredientDetailsStyles.ingredientdetails}>
+          <h2
+            className={
+              (ingredientDetailsStyles.header =
+                " text text_type_main-large mt-10 ml-10 mr-10")
+            }
+          >
+            Детали ингредиента
+          </h2>
+          <div className={ingredientDetailsStyles.content}>
+            <img src={ingredient.image_large} alt="Тут картинка из props" />
+            <p className="text text_type_main-medium mt-4">{ingredient.name}</p>
+            <ul
+              className={
+                ingredientDetailsStyles.parameters +
+                " text_color_inactive mt-8 mb-15"
+              }
+            >
+              <li className={ingredientDetailsStyles.parameter}>
+                <p className="text text_type_main-small">Каллории, ккал</p>
+                <p className="text text_type_digits-default mt-2">
+                  {ingredient.calories}
+                </p>
+              </li>
+              <li className={ingredientDetailsStyles.parameter}>
+                <p className="text text_type_main-small">Белки, г</p>
+                <p className="text text_type_digits-default mt-2">
+                  {ingredient.proteins}
+                </p>
+              </li>
+              <li className={ingredientDetailsStyles.parameter}>
+                <p className="text text_type_main-small">Жиры, г</p>
+                <p className="text text_type_digits-default mt-2">
+                  {ingredient.fat}
+                </p>
+              </li>
+              <li className={ingredientDetailsStyles.parameter}>
+                <p className="text text_type_main-small">Углеводы, г</p>
+                <p className="text text_type_digits-default mt-2">
+                  {ingredient.carbohydrates}
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
