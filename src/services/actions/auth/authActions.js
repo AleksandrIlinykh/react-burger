b46792ebfa92e28483078fc882e8be1d62f9e9cd
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2021-12-25 17:52:38 +0500

    Исправил в reducer GET_USER_INFO_ERROR

--- src/services/actions/auth/authActions.js
+++ src/services/actions/auth/authActions.js
@@ -221,11 +221,6 @@ export function logout() {
   };
 }
 
-/*
-export const GET_USER_INFO_REQUEST = "GET_USER_INFO_REQUEST";
-export const GET_USER_INFO_SUCCESS = "GET_USER_INFO_SUCCESS";
-export const GET_USER_INFO_ERROR = "GET_USER_INFO_ERROR";
-*/
 
 export function getUserInfo() {
   return function (dispatch) {
