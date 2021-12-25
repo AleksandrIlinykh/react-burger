86a88f6040f4017f2951067663feb6c16d9db20f
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2021-12-25 23:56:29 +0500

    =

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,8 +7,14 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { ProtectedRoute } from "../protected-route/protected-route ";
 import appStyles from "./app.module.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+import { hideIngredientDetails } from "../../services/actions/ingredient-details";
 import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
 import { getUserInfo } from "../../services/actions/auth/authActions";
 import LogIn from "../auth/log-in/log-in";
@@ -16,8 +22,10 @@ import Registration from "../auth/registration/registration";
 import PasswordRecovery from "../auth/password-recovery/password-recovery";
 import PasswordUpdating from "../auth/password-updating/password-updating";
 import Profile from "../profile/profile";
+import IngredientsDetails from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
 
-function App() {
+export default function App() {
   const dispatch = useDispatch();
 
   const isPasswordRecoverySucess = useSelector(
@@ -29,55 +37,107 @@ function App() {
     dispatch(getUserInfo());
   }, [dispatch]);
 
+  const ModalSwitch = () => {
+    const location = useLocation();
+    const history = useHistory();
+    let background = location.state && location.state.background;
+
+    const Main = () => {
+      return (
+        <>
+          <Switch>
+            <ProtectedRoute path="/login" forAuth={true} redirectTo={"/"}>
+              <LogIn />
+            </ProtectedRoute>
+            <ProtectedRoute
+              path="/registration"
+              forAuth={true}
+              redirectTo={"/"}
+            >
+              <Registration />
+            </ProtectedRoute>
+            <ProtectedRoute
+              path="/forgot-password"
+              forAuth={true}
+              redirectTo={"/"}
+            >
+              <PasswordRecovery />
+            </ProtectedRoute>
+            <ProtectedRoute
+              path="/password-updating"
+              forAuth={true}
+              redirectTo={"/"}
+              addPermissionCondition={isPasswordRecoverySucess}
+            >
+              <PasswordUpdating />
+            </ProtectedRoute>
+            <ProtectedRoute
+              path="/profile"
+              forAuth={false}
+              redirectTo={"/login"}
+            >
+              <Profile />
+            </ProtectedRoute>
+
+            <Route path="/">
+              <>
+                <DndProvider backend={HTML5Backend}>
+                  <div className={appStyles.content}>
+                    <div className={appStyles.contentleft}>
+                      <h1 className="text text_type_main-large mt-10 mb-5">
+                        {" "}
+                        Соберите бургер
+                      </h1>
+                      <BurgerIngredients />
+                    </div>
+                    <div className={appStyles.contentleft}>
+                      <BurgerConstructor />
+                    </div>
+                  </div>
+                </DndProvider>
+              </>
+            </Route>
+          </Switch>
+        </>
+      );
+    };
+    const handleModalClose = () => {
+      //dispatch(hideIngredientDetails());
+
+      history.goBack();
+    };
+
+    return (
+      <>
+        <AppHeader />
+        <Switch location={background || location}>
+          <Route path="/" exact>
+            <Main />
+          </Route>
+
+          <Route path="/ingredients/:ingredientId" exact>
+            <IngredientsDetails />
+          </Route>
+        </Switch>
+
+        {background && (
+          <Route
+            path="/ingredients/:ingredientId"
+            children={
+              <Modal handleModalClose={handleModalClose}>
+                <IngredientsDetails />
+              </Modal>
+            }
+          />
+        )}
+      </>
+    );
+  };
+
   return (
     <>
       <Router>
-        <AppHeader />
-        <Switch>
-          <ProtectedRoute path="/login" forAuth={true} redirectTo={"/"}>
-            <LogIn />
-          </ProtectedRoute>
-          <ProtectedRoute path="/registration" forAuth={true} redirectTo={"/"}>
-            <Registration />
-          </ProtectedRoute>
-          <ProtectedRoute
-            path="/forgot-password"
-            forAuth={true}
-            redirectTo={"/"}
-          >
-            <PasswordRecovery />
-          </ProtectedRoute>
-          <ProtectedRoute
-            path="/password-updating"
-            forAuth={true}
-            redirectTo={"/"}
-            addPermissionCondition={isPasswordRecoverySucess}
-          >
-            <PasswordUpdating />
-          </ProtectedRoute>
-          <ProtectedRoute path="/profile" forAuth={false} redirectTo={"/login"}>
-            <Profile />
-          </ProtectedRoute>
-
-          <Route path="/">
-            <>
-              <DndProvider backend={HTML5Backend}>
-                <div className={appStyles.content}>
-                  <div className={appStyles.contentleft}>
-                    <h1 className="text text_type_main-large mt-10 mb-5">
-                      {" "}
-                      Соберите бургер
-                    </h1>
-                    <BurgerIngredients />
-                  </div>
-                  <div className={appStyles.contentleft}>
-                    <BurgerConstructor />
-                  </div>
-                </div>
-              </DndProvider>
-            </>
-          </Route>
-        </Switch>
+        <ModalSwitch />
       </Router>
     </>
 
@@ -103,4 +163,3 @@ function App() {
   );
 }
 
-export default App;
