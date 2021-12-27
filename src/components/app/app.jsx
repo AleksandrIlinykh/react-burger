abf81d300f7875488de019dd70a42910e675f2a3
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2021-12-27 19:08:02 +0500

    Решил проблему с ошибкой react-dnd - вынес modal-switch в отделынй компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,44 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AppHeader from "../app-header/app-header";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { ProtectedRoute } from "../protected-route/protected-route ";
-import appStyles from "./app.module.css";
 import { getCookie } from "../../utils/cookies";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  useLocation,
-  useHistory,
-} from "react-router-dom";
-import { hideIngredientDetails } from "../../services/actions/ingredient-details";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
 import { getUserInfo } from "../../services/actions/auth/authActions";
 import { getRefreshToken } from "../../services/actions/auth/authActions";
 
-import LogIn from "../auth/log-in/log-in";
-import Registration from "../auth/registration/registration";
-import PasswordRecovery from "../auth/password-recovery/password-recovery";
-import PasswordUpdating from "../auth/password-updating/password-updating";
-import Profile from "../profile/profile";
-import IngredientsDetails from "../ingredient-details/ingredient-details";
-import Modal from "../modal/modal";
-
+import { ModalSwitch } from "../modal-switch/modal-switch";
 export default function App() {
   const dispatch = useDispatch();
 
-  const { isPasswordRecoverySucess, accessTokenExpired } = useSelector(
-    (store) => ({
-      isPasswordRecoverySucess: store.authData.isPasswordRecoverySucess,
-      getUserInfoSucess: store.authData.getUserInfoSucess,
-      accessTokenExpired: store.authData.accessTokenExpired,
-      refreshToken: store.authData.refreshToken,
-    })
-  );
+  const { accessTokenExpired } = useSelector((store) => ({
+    accessTokenExpired: store.authData.accessTokenExpired,
+  }));
 
   useEffect(() => {
     dispatch(getBurgerIngredients());
@@ -50,94 +25,6 @@ export default function App() {
         })
       );
   }, [dispatch, accessTokenExpired]);
-
-  const ModalSwitch = () => {
-    const location = useLocation();
-    const history = useHistory();
-    let background = location.state && location.state.background;
-
-    const handleModalClose = () => {
-      dispatch(hideIngredientDetails());
-
-      history.goBack();
-    };
-
-    return (
-      <>
-        <Switch location={background || location}>
-          <Route path="/ingredients/:ingredientId" exact>
-            <AppHeader />
-            <IngredientsDetails />
-          </Route>
-
-          <ProtectedRoute path="/login" forAuth={true} redirectTo={"/"}>
-            <AppHeader />
-            <LogIn />
-          </ProtectedRoute>
-
-          <ProtectedRoute path="/registration" forAuth={true} redirectTo={"/"}>
-            <AppHeader />
-            <Registration />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            path="/forgot-password"
-            forAuth={true}
-            redirectTo={"/"}
-          >
-            <AppHeader />
-            <PasswordRecovery />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            path="/password-updating"
-            forAuth={true}
-            redirectTo={"/"}
-            addPermissionCondition={isPasswordRecoverySucess}
-          >
-            <AppHeader />
-            <PasswordUpdating />
-          </ProtectedRoute>
-
-          <ProtectedRoute path="/profile" forAuth={false} redirectTo={"/login"}>
-            <AppHeader />
-            <Profile />
-          </ProtectedRoute>
-
-          <Route path="/">
-            <AppHeader />
-            <>
-              <DndProvider backend={HTML5Backend}>
-                <div className={appStyles.content}>
-                  <div className={appStyles.contentleft}>
-                    <h1 className="text text_type_main-large mt-10 mb-5">
-                      {" "}
-                      Соберите бургер
-                    </h1>
-                    <BurgerIngredients />
-                  </div>
-                  <div className={appStyles.contentleft}>
-                    <BurgerConstructor />
-                  </div>
-                </div>
-              </DndProvider>
-            </>
-          </Route>
-        </Switch>
-
-        {background && (
-          <Route
-            path="/ingredients/:ingredientId"
-            children={
-              <Modal handleModalClose={handleModalClose}>
-                <IngredientsDetails />
-              </Modal>
-            }
-          />
-        )}
-      </>
-    );
-  };
 
   return (
     <>
---
+++ src/components/modal-switch/modal-switch.jsx
@@ -0,0 +1,117 @@
+import AppHeader from "../app-header/app-header";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
+import { ProtectedRoute } from "../protected-route/protected-route ";
+import appStyles from "./app.module.css";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+import { hideIngredientDetails } from "../../services/actions/ingredient-details";
+
+import LogIn from "../auth/log-in/log-in";
+import Registration from "../auth/registration/registration";
+import PasswordRecovery from "../auth/password-recovery/password-recovery";
+import PasswordUpdating from "../auth/password-updating/password-updating";
+import Profile from "../profile/profile";
+import IngredientsDetails from "../ingredient-details/ingredient-details";
+import Modal from "../modal/modal";
+
+export const ModalSwitch = () => {
+  const location = useLocation();
+  const history = useHistory();
+  let background = location.state && location.state.background;
+  const dispatch = useDispatch();
+  const { isPasswordRecoverySucess, accessTokenExpired } = useSelector(
+    (store) => ({
+      isPasswordRecoverySucess: store.authData.isPasswordRecoverySucess,
+      getUserInfoSucess: store.authData.getUserInfoSucess,
+      accessTokenExpired: store.authData.accessTokenExpired,
+      refreshToken: store.authData.refreshToken,
+    })
+  );
+  const handleModalClose = () => {
+    dispatch(hideIngredientDetails());
+
+    history.goBack();
+  };
+
+  return (
+    <>
+      <Switch location={background || location}>
+        <Route path="/ingredients/:ingredientId" exact>
+          <AppHeader />
+          <IngredientsDetails />
+        </Route>
+
+        <ProtectedRoute path="/login" forAuth={true} redirectTo={"/"}>
+          <AppHeader />
+          <LogIn />
+        </ProtectedRoute>
+
+        <ProtectedRoute path="/registration" forAuth={true} redirectTo={"/"}>
+          <AppHeader />
+          <Registration />
+        </ProtectedRoute>
+
+        <ProtectedRoute path="/forgot-password" forAuth={true} redirectTo={"/"}>
+          <AppHeader />
+          <PasswordRecovery />
+        </ProtectedRoute>
+
+        <ProtectedRoute
+          path="/password-updating"
+          forAuth={true}
+          redirectTo={"/"}
+          addPermissionCondition={isPasswordRecoverySucess}
+        >
+          <AppHeader />
+          <PasswordUpdating />
+        </ProtectedRoute>
+
+        <ProtectedRoute path="/profile" forAuth={false} redirectTo={"/login"}>
+          <AppHeader />
+          <Profile />
+        </ProtectedRoute>
+
+        <Route path="/">
+          <AppHeader />
+          <>
+            <DndProvider backend={HTML5Backend}>
+              <div className={appStyles.content}>
+                <div className={appStyles.contentleft}>
+                  <h1 className="text text_type_main-large mt-10 mb-5">
+                    {" "}
+                    Соберите бургер
+                  </h1>
+                  <BurgerIngredients />
+                </div>
+                <div className={appStyles.contentleft}>
+                  <BurgerConstructor />
+                </div>
+              </div>
+            </DndProvider>
+          </>
+        </Route>
+      </Switch>
+
+      {background && (
+        <Route
+          path="/ingredients/:ingredientId"
+          children={
+            <Modal handleModalClose={handleModalClose}>
+              <IngredientsDetails />
+            </Modal>
+          }
+        />
+      )}
+    </>
+  );
+};
