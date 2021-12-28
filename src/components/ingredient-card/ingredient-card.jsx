8b894b85074a2e45e29da24985d16aa8e54007d7
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2021-12-28 11:21:25 +0500

    деструктурировал props, добавил uuid key для перетаскиваемого ингредиента

--- src/components/ingredient-card/ingredient-card.jsx
+++ src/components/ingredient-card/ingredient-card.jsx
@@ -8,8 +8,20 @@ import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ingredientCardTypes } from "../../utils/types";
 import ingredientCardStyles from "./ingredient-card.module.css";
 import { showIngredientDetails } from "../../services/actions/ingredient-details";
+import { v4 as uuidv4 } from "uuid";
 
-const IngredientCard = (props) => {
+const IngredientCard = ({
+  calories,
+  carbohydrates,
+  fat,
+  image,
+  image_large,
+  name,
+  price,
+  proteins,
+  type,
+  _id,
+}) => {
   const [orderCount, setOrderCount] = React.useState(0);
 
   const { sausesAndFillings, bun } = useSelector((store) => ({
@@ -19,21 +31,34 @@ const IngredientCard = (props) => {
 
   useEffect(() => {
     const theSameIngredientsAmount =
-      sausesAndFillings.filter((ingredient) => ingredient._id === props._id)
-        .length +
-      (bun._id === props._id);
+      sausesAndFillings.filter((ingredient) => ingredient._id === _id).length +
+      (bun._id === _id);
     setOrderCount(theSameIngredientsAmount);
-  }, [sausesAndFillings.length, bun._id, props._id, sausesAndFillings]);
+  }, [sausesAndFillings.length, bun._id, _id, sausesAndFillings]);
 
   const dispatch = useDispatch();
 
   function handleClick(event) {
-    dispatch(showIngredientDetails(props._id));
+    dispatch(showIngredientDetails(_id));
   }
 
   const [{ isDrag }, dragRef] = useDrag({
     type: "ingredient",
-    item: { ingredient: props },
+    item: {
+      ingredient: {
+        calories: calories,
+        carbohydrates: carbohydrates,
+        fat: fat,
+        image: image,
+        image_large: image_large,
+        name: name,
+        price: price,
+        proteins: proteins,
+        type: type,
+        _id: _id,
+        key: uuidv4(),
+      },
+    },
     collect: (monitor) => ({
       isDrag: monitor.isDragging(),
     }),
@@ -47,11 +72,11 @@ const IngredientCard = (props) => {
 
   return (
     <Link
-      key={props._id}
+      key={_id}
       to={{
         // Тут мы формируем динамический путь для нашего ингредиента
         // а также сохраняем в свойство background роут, на котором была открыта наша модалка.
-        pathname: `/ingredients/${props._id}}`,
+        pathname: `/ingredients/${_id}}`,
         state: { background: location },
       }}
       //className={styles.link}
@@ -63,16 +88,16 @@ const IngredientCard = (props) => {
       >
         <div>
           {orderCount === 0 || <Counter count={orderCount} size="default" />}
-          <img src={props.image} alt={props.name} className="ml-4 mr-4" />
+          <img src={image} alt={name} className="ml-4 mr-4" />
         </div>
         <div className="mt-1 mb-1">
           <div className={ingredientCardStyles.ingredientcard__price}>
-            <p className="text text_type_digits-default">{props.price}</p>
+            <p className="text text_type_digits-default">{price}</p>
             <CurrencyIcon type="primary" />
           </div>
         </div>
         <div className={ingredientCardStyles.ingredientcard__description}>
-          <p className="text text_type_main-small">{props.name}</p>
+          <p className="text text_type_main-small">{name}</p>
         </div>
       </div>
     </Link>
