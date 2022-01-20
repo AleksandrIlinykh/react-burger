9a3ddaf43dc0c3c72b0a1f4410bbf824adcb1a99
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-20 11:13:36 +0500

    setted logo as link ti home directory

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
-import NavigationElement from '../navigation-element/navigation-element'
+import { NavLink } from "react-router-dom";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import NavigationElement from "../navigation-element/navigation-element";
 
-import appHeaderStyles from './app-header.module.css';
+import appHeaderStyles from "./app-header.module.css";
 
 function AppHeader() {
-
-    return (
-      <header>
-        <div className={appHeaderStyles.header}>
-          <div className={appHeaderStyles.header__container}>
-            <div className={appHeaderStyles.header__leftlinks}>
-              <div className="mr-2">
-                <NavigationElement
-                  exact={true}
-                  type="constructor"
-                  name="Конструктор"
-                  path="/"
-                  isActive
-                />
-              </div>
+  return (
+    <header>
+      <div className={appHeaderStyles.header}>
+        <div className={appHeaderStyles.header__container}>
+          <div className={appHeaderStyles.header__leftlinks}>
+            <div className="mr-2">
               <NavigationElement
-                type="order list"
-                name="Лента заказов"
-                path="/orderlist"
+                exact={true}
+                type="constructor"
+                name="Конструктор"
+                path="/"
                 isActive
               />
             </div>
+            <NavigationElement
+              type="order list"
+              name="Лента заказов"
+              path="/orderlist"
+              isActive
+            />
+          </div>
+          <NavLink exact to={`/`}>
             <Logo />
-            <div className={appHeaderStyles.header__rightlinks}>
-              <NavigationElement
-                type="personal cabinet"
-                name="Личный кабинет"
-                path="/profile"
-                isActive
-              />
-            </div>
+          </NavLink>
+
+          <div className={appHeaderStyles.header__rightlinks}>
+            <NavigationElement
+              type="personal cabinet"
+              name="Личный кабинет"
+              path="/profile"
+              isActive
+            />
           </div>
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  );
 }
 
 export default AppHeader;
