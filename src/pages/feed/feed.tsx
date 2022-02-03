15530014f9f70112b5b309ce2e37c541d1294064
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-02-03 13:27:33 +0500

    updated feed

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -8,6 +8,11 @@ import feedStyles from "./feed.module.css";
 
 export default function Feed() {
   const dispatch = useDispatch();
+  const { orders, isDoneToday, isDoneAllTime } = useSelector((store: any) => ({
+    orders: store.ws.orders,
+    isDoneToday: store.ws.messages[store.ws.messages.length - 1]?.totalToday,
+    isDoneAllTime: store.ws.messages[store.ws.messages.length - 1]?.total,
+  }));
 
   useEffect(() => {
     dispatch(wsInit());
@@ -16,17 +21,61 @@ export default function Feed() {
     };
   }, [dispatch]);
 
+  const readyOrderNumbers = () => {
+    return orders
+      ?.filter((order: any) => order.status === "done")
+      .map((order: any) => (
+        <p className="text text_type_digits-default mb-2 mr-4">
+          {order.number}
+        </p>
+      ));
+  };
+
+  const inProgressOrderNumbers = () => {
+    return orders
+      ?.filter((order: any) => order.status !== "done")
+      .map((order: any) => (
+        <p className="text text_type_digits-default mb-2 mr-4">
+          {order.number}
+        </p>
+      ));
+  };
+
   return (
     <>
       <AppHeader />
       <div className={feedStyles.content}>
-        <div className={feedStyles.contentleft}>
-          <h1 className="text text_type_main-large mt-10 mb-5">
-            Лента заказов
-          </h1>
-          <OrderContainer></OrderContainer>
+        <h1 className="text text_type_main-large mt-10 mb-5">Лента заказов</h1>
+        <div className={feedStyles.container}>
+          <div className={feedStyles.contentleft}>
+            <OrderContainer></OrderContainer>
+          </div>
+          <div className={feedStyles.contentleft}>
+            <div className={feedStyles.status + " mb-15"}>
+              <div className={feedStyles.done}>
+                <h3 className="text text_type_main-medium mb-6">Готовы:</h3>
+                <div className={feedStyles.statusContainer}>
+                  {readyOrderNumbers()}
+                </div>
+              </div>
+              <div className={feedStyles.inProgress}>
+                <div className={feedStyles.done}>
+                  <h3 className="text text_type_main-medium mb-6">В работе:</h3>
+                  <div className={feedStyles.statusContainer}>
+                    {inProgressOrderNumbers()}
+                    <div className="mb-2"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h2 className="text text_type_main-large">
+              Выполнено за всё время:
+            </h2>
+            <p className="text text_type_digits-large mb-15">{isDoneAllTime}</p>
+            <h2 className="text text_type_main-large">Выполнено за сегодня:</h2>
+            <p className="text text_type_digits-large">{isDoneToday}</p>
+          </div>
         </div>
-        <div className={feedStyles.contentleft}></div>
       </div>
     </>
   );
